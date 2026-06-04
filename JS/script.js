b2723f87dcb7de87925b5ee528bf1423efab0332
selvakumar <selvakumar.v2008@gmail.com>
2026-06-04 15:40:12 +0530

    css completed and js uploaded

---
+++ JS/script.js
@@ -0,0 +1,83 @@
+// var a =10;//Re-declaration and Re-assignment allowed
+// let b=20;//re-assignment allowed but re-declaration not allowed
+// const c=30;//re-assignment and re-declaration not allowed
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// var d =100;
+// var e="hello world";
+// var boolean = false;
+// var undefined;
+// var Null = null;
+// console.log(typeof d);
+// console.log(typeof e);
+// console.log(typeof boolean);
+// console.log(typeof undefined);
+// console.log(typeof Null);
+
+// var bigInt = 12345n;
+// var symbol = Symbol('li');
+// console.log(typeof bigInt);
+// console.log(typeof symbol);
+
+// var arr=[10,20,30,40,50];
+// console.log(typeof arr,arr);
+ 
+// var obj={
+//     name:"sk",
+//     age:22,
+//     dept:["ct","ai&DS","Css"]
+// }
+// console.log(typeof obj,obj);
+
+//arithmetic operator(+,-,*,/,%,)
+// var a =10;
+// var b=20;
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// var c=15
+// var e="30"
+// console.log(c+e)
+// console.log(c-e)
+// var d="sk"
+// console.log(d+c)
+// console.log(d-c)
+// //Logical operator &&,||,!
+// var a=true;
+// var b=false;
+
+// console.log(a&&b);
+// console.log(a||b);
+// console.log(!a);
+// console.log("Relation or comparision operator < > <=  >=  == !=");
+// a=10;
+// b="20";
+// console.log(a<b);
+// console.log(a>b);
+// console.log(a<=b);
+// console.log(a>=b);
+// console.log(a==b);
+// console.log(a!=b);
+// console.log(a===b);
+// console.log(a!==b);
+// console.log("Assingment operator");
+// var a=10;
+// var b=20;
+// var a=a+b;
+// var a+= b;
+// console.log(a);
+// console.log("Unary Operator");
+// var a =10;
+// var b =20;
+// console.log(a++);
+// console.log(++a);
+// console.log(--b);
+// console.log(b--);
+a=1;
+b=1;
+c=0;
+result=a++ + --b + ++c - ++a + ++b + c++ + b++ + a;
+console.log(result);
